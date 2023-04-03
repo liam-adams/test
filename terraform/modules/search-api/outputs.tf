0df31e8be657046121fe89f7bf9ef6602c7f303c
Liam Adams <liam_adams@mckinsey.com>
2023-04-02 22:13:52 -0500

    add output

--- terraform/modules/search-api/outputs.tf
+++ terraform/modules/search-api/outputs.tf
@@ -2,3 +2,8 @@ output redis_host {
     description = "hostname of redis cluster"
     value = module.elasticache-redis.host
 }
+
+output ecr_repo_url {
+    description = "url of ecr repo"
+    value = module.ecr.repository_url
+}
